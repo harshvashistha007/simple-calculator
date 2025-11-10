191c94ad904dc9e8a2839245f8d05c176ba07049
Harsh Vashistha <07harshvashistha@gmail.com>
2025-11-10 15:18:40 +0530

    Add files via upload

---
+++ harshg.py
@@ -0,0 +1,99 @@
+'''print(444444)
+
+print(123445)'''
+
+
+'''a=2
+b=5
+
+
+print(a+b)'''
+'''name=input("name")
+age=int(input("age?"))
+print(name)
+
+
+print(age)'''
+
+
+
+'''a=int(input("enterrn any no."))
+b=int(input("enterrn any no."))
+print(a+b)
+print(a*b)
+print(a-b)
+print(a/b)'''
+
+
+
+'''a=int(input("enterrn any roll no."))
+b=input("enterrn name.")
+c=input("class")
+d=input("enter father name")
+e=input("address")
+f=input("city")
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)'''
+
+
+'''a=int(input("enter your age"))
+if a>=18:
+    print("elliogible")
+else:
+    print("not elligible")'''
+
+
+'''a=int(input("enter any"))
+b=int(input("enter any"))
+if a+b==23:
+    print("done")
+elif a+b==25:
+    print("you great")
+else:
+    print("no")'''
+
+''''a=["werty","12345","ertyiu",]
+print(a[::-1])'''
+
+
+'''a=["1","2","3","4","5","6","7","8","9",]
+print(a[3:-1:])'''
+
+
+
+'''a={"name":"adarsh","branch":"ai"}
+print(a["branch"])'''
+
+
+
+'''a=("abcd","12344","rtrt54",)
+
+print(a[::-1])'''
+
+
+
+b=int(input("enter any second"))
+
+choice=input("enter you choice +,-,*,/")
+
+if choice=="+":
+    print(a+b)
+elif choice=="-":
+    print(a-b)
+elif choice=="/":
+    print(a/b)
+elif choice=="*":
+    print(a*b)
+else:
+    print("invalid")
+
+
+
+
+
+
+
